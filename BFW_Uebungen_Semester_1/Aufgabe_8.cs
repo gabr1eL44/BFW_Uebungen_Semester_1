fa828ddda7ae0ad7b4aadca01e7367be5fccbf5f
gabr1eL44 <gabrielinc@gmx.de>
2022-02-16 15:54:16 +0100

    Kommentare ergänzt

--- BFW_Uebungen_Semester_1/Aufgabe_8.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_8.cs
@@ -10,17 +10,21 @@ namespace BFW_Uebungen_Semester_1
         {
             string inputString = " werPolizeidjsue ";
 
+            // Entferne Leerzeichen am Anfang und Ende
             inputString = inputString.Trim();
             Console.WriteLine(inputString);
 
+            // Gibt die Länge des Strings aus
             int n = inputString.Length;
             Console.WriteLine("Anzahl: {0}", n);
 
+            // Entnimmt nur den Teil "Polizei" aus String
             inputString = inputString.Substring(3, 7);
             Console.WriteLine(inputString);
 
+            // Wandelt alles in Großbuchstaben
             inputString = inputString.ToUpper();
-            Console.WriteLine(inputString);
+            Console.WriteLine(inputString);           
         }
     }
 }
