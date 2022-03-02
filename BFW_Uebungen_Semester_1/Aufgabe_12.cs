9cf32a88b11d7cfe718d7bb2c19e5c8a005def2a
gabr1eL44 <gabrielinc@gmx.de>
2022-03-02 12:44:51 +0100

    Aufgabe 12

--- BFW_Uebungen_Semester_1/Aufgabe_12.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_12.cs
@@ -8,13 +8,32 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
-            Console.Write("Bitte geben Sie Ihre Zahl ein: ");
+            int[] numbers = new int[4] {70, 30, 89, 13};
 
-            string input = Console.ReadLine();
-            int number = Convert.ToInt32(input);
+            numbers[2] = 110;
+            numbers[3] += 17;
 
-            Console.Clear();
-            Console.WriteLine("Ihre eigegebene Zahl: " + number);
+            for (int i = 0; i<4; i++)
+            {
+                numbers[i] /= 5;
+            }
+
+            int[] newNumbers = new int[6];
+            for (int i = 0; i<4; i++)
+            {
+                newNumbers[i] = numbers[i];
+            }
+            newNumbers[4] = 200; newNumbers[5] = 500;
+            
+            for (int i = 0; i<6; i++)
+            {
+                newNumbers[i] *= 2;
+            }
+            
+            for (int i = 0; i<6; i++)
+            {
+                Console.WriteLine(i+1 + "tes Element: " + newNumbers[i]);
+            }          
         }
     }
 }
