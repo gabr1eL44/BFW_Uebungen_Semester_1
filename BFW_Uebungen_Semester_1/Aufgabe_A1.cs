3e6dc6dcea05a5a1b9b7913a814f6a46d36e9c04
gabr1eL44 <gabrielinc@gmx.de>
2022-03-02 18:04:30 +0100

    fixed

--- BFW_Uebungen_Semester_1/Aufgabe_A1.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_A1.cs
@@ -7,12 +7,10 @@ namespace BFW_Uebungen_Semester_1
         public static void Start()
         {
             Console.Write("Bitte geben Sie die erste Farbe ein: ");
-            string farbe1 = Console.ReadLine().Trim();
+            string farbe1 = Console.ReadLine().Trim().ToLower();
 
             Console.Write("Bitte geben Sie die zweite Farbe ein: ");
-            string farbe2 = Console.ReadLine().Trim();
-
-            farbe1 = farbe1.ToLower(); farbe2 = farbe2.ToLower();
+            string farbe2 = Console.ReadLine().Trim().ToLower();
 
             if (((farbe1 == "blau") || (farbe2 == "blau")) && ((farbe1 == "rot") || (farbe2 == "rot")))
                 Console.WriteLine("Ihre Mischfarbe ist Lila!");
@@ -21,7 +19,7 @@ namespace BFW_Uebungen_Semester_1
                 Console.WriteLine("Ihre Mischfarbe ist Orange!");
 
             else if (((farbe1 == "gelb") || (farbe2 == "gelb")) && ((farbe1 == "blau") || (farbe2 == "blau")))
-                Console.WriteLine("Ihre Mischfarbe ist Grün!");
+                Console.WriteLine("\nIhre Mischfarbe ist Grün!");
 
             else
                 Console.WriteLine("\nFehlerhafte Eingabe bzw. geben Sie zwei verschiedene Farben ein!");
