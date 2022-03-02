bd9ab19b1c2f2d9c702f4168f04a5b5a0b4cbbee
gabr1eL44 <gabrielinc@gmx.de>
2022-03-02 14:00:13 +0100

    fix

--- BFW_Uebungen_Semester_1/Aufgabe_12.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_12.cs
@@ -11,24 +11,21 @@ namespace BFW_Uebungen_Semester_1
             int[] numbers = new int[4] {70, 30, 89, 13};
 
             numbers[2] = 110; numbers[3] += 17;
-            for (int i = 0; i<4; i++) {
+            for (int i = 0; i<4; i++) 
                 numbers[i] /= 5;
-            }
-
+            
             int[] newNumbers = new int[6];
 
-            for (int i = 0; i<4; i++) {
+            for (int i = 0; i<4; i++)
                 newNumbers[i] = numbers[i];
-            }
+            
             newNumbers[4] = 200; newNumbers[5] = 500;
             
-            for (int i = 0; i<6; i++) {
+            for (int i = 0; i<6; i++)
                 newNumbers[i] *= 2;
-            }
-            
-            for (int i = 0; i<6; i++) {
+                        
+            for (int i = 0; i<6; i++) 
                 Console.WriteLine("{0}tes Element: " + newNumbers[i], (i + 1));
-            }          
         }
     }
 }
