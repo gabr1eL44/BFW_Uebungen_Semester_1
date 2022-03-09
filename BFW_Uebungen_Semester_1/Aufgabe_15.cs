ba0c4888bc7e89e8d1a8795dc55dff94c7057e24
gabr1eL44 <gabrielinc@gmx.de>
2022-03-09 12:51:30 +0100

    Task 15 added

--- BFW_Uebungen_Semester_1/Aufgabe_15.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_15.cs
@@ -1,13 +1,37 @@
 using System;
-using System.Threading;
 
 namespace BFW_Uebungen_Semester_1
 {
     class Aufgabe_15
     {
-        public static void Start()
+        public static async void Start()
         {
+            string inputString; 
+            int inputInt;
+
+            do
+            {
+                Console.Write("Bitte geben Sie eine Zahl ein: ");
+                inputString = Console.ReadLine().Trim();
+
+                if (int.TryParse(inputString, out inputInt))
+                    break;
+                else
+                {
+                    Console.WriteLine("\nFehlerhafte Eingabe. Bitte eine Zahl eingeben!\n");
+                    if (inputString.Length % 2 == 0)
+                        Console.WriteLine(inputString.Substring(0, (inputString.Length / 2)) + " | " + inputString.Substring((inputString.Length / 2)) + "\n");
+                    else
+                        Console.WriteLine(inputString.Substring(0, (inputString.Length / 2)) + " | " + inputString[inputString.Length / 2] + " | " + inputString.Substring((inputString.Length / 2) + 1) + "\n");
+                }
+            } while (true);
             
+            Console.Clear();
+
+            if (inputInt % 2 == 0)
+                Console.WriteLine("Zahl " + inputInt + " geteilt durch 2 (ohne Rest): " + (inputInt / 2));
+            else
+                Console.WriteLine("Zahl " + inputInt + " geteilt durch 2 (mit Rest): " + (inputInt / 2) + " Rest " + (inputInt % 2));
         }
     }
 }
