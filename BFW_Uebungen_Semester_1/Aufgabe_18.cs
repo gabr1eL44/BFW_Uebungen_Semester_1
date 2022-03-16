cee68f5a50b72d85909376b40bd506f08141a2ca
gabr1eL44 <gabrielinc@gmx.de>
2022-03-16 14:57:16 +0100

    task 18 completed

--- BFW_Uebungen_Semester_1/Aufgabe_18.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_18.cs
@@ -6,7 +6,28 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
+            Console.WriteLine("Bitte geben Sie eine Begrüßung ein:");
+            string eingabe = Console.ReadLine();
 
+            switch(eingabe.Trim().ToLower())
+            {
+                case "hallo":
+                    Console.WriteLine("Hi!");
+                    Console.WriteLine("Die Begrüßung '" + eingabe + "' kenne ich :D");
+                    break;
+                case "hi":
+                    Console.WriteLine("Servus!");
+                    Console.WriteLine("Die Begrüßung '" + eingabe + "' kenne ich :D");
+                    break;
+                case "servus":
+                    Console.WriteLine("Hallo!");
+                    Console.WriteLine("Die Begrüßung '" + eingabe + "' kenne ich :D");
+                    break;
+                default:
+                    Console.WriteLine("Hi!");
+                    Console.WriteLine("Leider kenne ich die Begrüßung nicht :(");
+                    break;
+            }
         }
     }
 }
---
+++ BFW_Uebungen_Semester_1/Aufgabe_19.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_19
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
