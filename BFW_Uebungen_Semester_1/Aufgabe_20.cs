580dbe56067f12746fba42b634eacd6813560f17
gabr1eL44 <gabrielinc@gmx.de>
2022-03-19 09:36:08 +0100

    new task 20

---
+++ BFW_Uebungen_Semester_1/Aufgabe_20.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_20
+    {
+        public static void Start()
+        {
+ 
+        }
+    }
+}
