0b08e5e35da66a96a2bd29490a81bacc031d2774
gabr1eL44 <gabrielinc@gmx.de>
2022-03-21 15:13:10 +0100

    slight change

--- BFW_Uebungen_Semester_1/Aufgabe_21.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_21.cs
@@ -6,12 +6,12 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
-            int[] numbers = new int[0];
+            int[] numbers = new int[0]; string eingabe = "y";
 
-            while (true)
+            while (eingabe == "y")
             {
                 Console.WriteLine("Bitte geben Sie eine Zahl ein:");
-                string eingabe = Console.ReadLine().Trim();
+                eingabe = Console.ReadLine().Trim();
 
                 if (int.TryParse(eingabe, out int id))
                 {
@@ -26,20 +26,10 @@ namespace BFW_Uebungen_Semester_1
                 {
                     Console.WriteLine("Soll noch eine Zahl eingegen werden (y/n)");
                     eingabe = Console.ReadLine();
-
-                    if (eingabe == "n")
-                        goto Ausgabe;
-                    else if (eingabe == "y")
-                        break;
-                    else
-                        Console.Write("Ungültige Eingabe. ");
-                } while (true);
+                } while ((eingabe != "y") && (eingabe != "n"));
             }
 
-            // Sprungmarke um aus Doppelschleife auszubrechen
-            Ausgabe:
             int sum = 0;
-
             Console.WriteLine("\nIhr Ergebnis:");
             for (int i = 0; i < numbers.Length; i++)
             {
