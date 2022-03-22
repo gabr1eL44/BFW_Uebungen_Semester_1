cf7d0de486656d27af2ad79b5051640c2d0802bb
gabr1eL44 <gabrielinc@gmx.de>
2022-03-22 07:26:16 +0100

    task 23 completed

---
+++ BFW_Uebungen_Semester_1/Aufgabe_22.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_22
+    {
+        public static void Start()
+        {
+            int sum = 0; int[] numbers = new int[0];
+
+            Console.WriteLine("Bitte Zahlen eingeben und mit Enter bestätigen:");
+            do
+            {
+                 string eingabe = Console.ReadLine().Trim();
+
+                if (int.TryParse(eingabe, out int id))
+                {
+                    int[] tempArr = new int[numbers.Length + 1];
+                    for (int i = 0; i < numbers.Length; i++)
+                        tempArr[i] = numbers[i];
+                    tempArr[numbers.Length] = id;
+                    numbers = tempArr;
+                }
+
+                sum += numbers[numbers.Length - 1];
+            } while (sum <= 100);
+            Console.WriteLine("\nDie Summe der Eingaben: " + sum);
+            Console.WriteLine("Die Anzahl der Eingaben, bis die 100 überschritten wurde: " + numbers.Length);
+        }
+    }
+}
---
+++ BFW_Uebungen_Semester_1/Aufgabe_23.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_23
+    {
+        public static void Start()
+        {
+            string eingabe; string ausgabe = "";
+
+            Console.WriteLine("Bitte geben Sie String ein:");
+            do
+            {
+                eingabe = Console.ReadLine().Trim();
+                if (eingabe.Length == 0)
+                    break;
+                else if (eingabe.Length < 6)
+                    if (ausgabe != "")
+                        ausgabe += " und " + eingabe;
+                    else
+                        ausgabe += eingabe;
+                else
+                {
+                    int currentPos = 0; string tempString = ""; bool lastCharAllowed = false;
+                    for (int i = 0; i < eingabe.Length; i++)
+                    {
+                        switch (eingabe.ToLower()[i])
+                        {
+                            case 'a':
+                            case 'e':
+                            case 'i':
+                            case 'o':
+                            case 'u':
+                                tempString += eingabe.Substring(currentPos, i - currentPos);
+                                currentPos = i + 1;
+                                break;
+                            default:
+                                if (i + 1 == eingabe.Length)
+                                    lastCharAllowed = true;
+                                break;
+                        }
+                    }
+                    if (lastCharAllowed)
+                        tempString += eingabe.Substring(currentPos);
+
+                    if (tempString.Length > 5)
+                    {                
+                        currentPos = 0; string newTempString = ""; lastCharAllowed = false;
+                        for (int i = 0; i < tempString.Length; i++)
+                        {
+                            switch (tempString.ToLower()[i])
+                            {
+                                case 's':
+                                case 't':
+                                case 'd':
+                                    newTempString += tempString.Substring(currentPos, i - currentPos);
+                                    currentPos = i + 1;
+                                    break;
+                                default:
+                                    if (i + 1 == tempString.Length)
+                                        lastCharAllowed = true;
+                                    break;
+                            }
+                        }
+                        if (lastCharAllowed)
+                            newTempString += tempString.Substring(currentPos);
+                        tempString = newTempString;
+                    }
+                    if (ausgabe != "")
+                        ausgabe += " und " + tempString;
+                    else
+                        ausgabe += tempString;
+                }
+            } while (true);
+
+            Console.WriteLine("Ihr String ist: " + ausgabe);
+        }
+    }
+}
