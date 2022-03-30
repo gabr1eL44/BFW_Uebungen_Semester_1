4a6865837e14fb5e7decf970deb5e6d82e2dc78e
gabr1eL44 <gabrielinc@gmx.de>
2022-03-30 16:07:03 +0200

    fix

--- BFW_Uebungen_Semester_1/Aufgabe_24.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_24.cs
@@ -6,26 +6,6 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
-            int[] numbers = new int[0];
-
-            Array.Add(ref numbers, 67);
-            Array.Clear(ref numbers);
-
-            numbers = Array.Add(numbers, 67);
-            numbers = Array.Clear(numbers);
-
-            Array.Add(ref numbers, 5);
-            Array.Add(ref numbers, 4);
-            numbers = Array.Add(numbers, 8);
-            numbers = Array.Add(numbers, 25);
-            Array.Add(ref numbers, 47);
-            Array.Add(ref numbers, 98);
-
-            Array.Remove(ref numbers, 3);
-            numbers = Array.Remove(numbers, 3);
-
-            for (int i = 0; i < numbers.Length; i ++)
-                Console.WriteLine("{0}. Eintrag : {1}", (i + 1), numbers[i]);
         }
     }
 }
