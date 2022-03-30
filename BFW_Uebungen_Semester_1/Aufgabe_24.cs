1b0e8656cfcf1e26e763587b31dccc9ac7206237
gabr1eL44 <gabrielinc@gmx.de>
2022-03-30 14:28:29 +0200

    new task 24

---
+++ BFW_Uebungen_Semester_1/Aufgabe_24.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_24
+    {
+        public static void Start()
+        {
+            int number;
+
+            Console.Write("Bitte Eingabe: ");
+            string eingabe = Console.ReadLine();
+
+            if (!int.TryParse(eingabe, out number))
+                number = -1;
+
+            Console.WriteLine("Ihre Zahl: " + number);
+
+        }
+    }
+}
