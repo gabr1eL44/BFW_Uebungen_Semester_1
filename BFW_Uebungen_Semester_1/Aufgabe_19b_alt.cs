8f019471c721733eca451ab71ebfb127cb69e273
gabr1eL44 <gabrielinc@gmx.de>
2022-03-30 15:35:23 +0200

    New Aufgabe_19b_alt

---
+++ BFW_Uebungen_Semester_1/Aufgabe_19b_alt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_19b_alt
+    {
+        public static void Start()
+        {
+            int[] numbers = new int[] { 64, 32, 16, 48, 2 };
+
+            do
+            {
+                Console.Write("Liste (a)usgeben, Element (h)inzufügen, Liste (l)eeren, Programm (b)eenden?");
+                char input = Console.ReadKey().KeyChar;
+
+                switch (input)
+                {
+                    case 'a':
+                        Console.WriteLine();
+                        for (int i = 0; i < numbers.Length; i++)
+                            Console.Write(numbers[i] + " ");
+                        Console.WriteLine(": Länge({0})", numbers.Length);
+                        break;
+                    case 'h':
+                        Console.Write("\nBitte geben Sie eine neue Zahl ein: ");
+                        if (int.TryParse(Console.ReadLine(), out int zahl))
+                            Array.Add(ref numbers, zahl);
+                        else
+                            Console.WriteLine("Fehlerhafte Eingabe!");
+                        break;
+                    case 'l':
+                        Array.Clear(ref numbers);
+                        Console.WriteLine();
+                        break;
+                    case 'b':
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.WriteLine();
+            } while (true);
+        }
+    }
+}
