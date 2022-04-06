85072ed25c00fae1e53f7b5b43a8ae64b3305525
gabr1eL44 <gabrielinc@gmx.de>
2022-04-06 16:12:44 +0200

    fix

--- BFW_Uebungen_Semester_1/Program.cs
+++ BFW_Uebungen_Semester_1/Program.cs
@@ -7,7 +7,7 @@ namespace BFW_Uebungen_Semester_1
         static void Main()
         {
             // Start Current Task
-            Aufgabe_20.Start();
+            Aufgabe_21.Start();
 
             // Wait for KeyPress
             Console.WriteLine("\nDrücken Sie eine beliebige Taste, um dieses Fenster zu schließen.");
