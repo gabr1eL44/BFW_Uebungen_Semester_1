978c7010c559f8e9aac3e115bd96626f4e55808d
gabr1eL44 <gabrielinc@gmx.de>
2022-04-16 08:25:34 +0200

    uebungen

--- BFW_Uebungen_Semester_1/Uebungen.cs
+++ BFW_Uebungen_Semester_1/Uebungen.cs
@@ -6,7 +6,20 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
+            Console.Write("Bitte geben Sie den gewünschten Betrag ein (€ oder $): ");
+            string str = Console.ReadLine();
 
+            char currency = str.Trim()[^1];
+            bool error = false;
+            if (!decimal.TryParse(str.Trim()[..^1], out decimal value))
+                error = true;
+
+            if (currency == '€' && !error)
+                Console.WriteLine("Es wurden {0} Euro eingegeben!", value);
+            else if (currency == '$' && !error)
+                Console.WriteLine("Es wurden {0} Dollar eingegeben!", value);
+            else
+                Console.WriteLine("Fehlerhafte Eingabe!");
         }
     }
 }
