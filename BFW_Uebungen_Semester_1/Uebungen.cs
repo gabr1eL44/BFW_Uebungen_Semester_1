340b4bfdc140dc10324f41c9c009035fa0671af5
gabr1eL44 <gabrielinc@gmx.de>
2022-04-22 14:35:52 +0200

    global test

--- BFW_Uebungen_Semester_1/Uebungen.cs
+++ BFW_Uebungen_Semester_1/Uebungen.cs
@@ -2,8 +2,16 @@
 
 namespace BFW_Uebungen_Semester_1
 {
+    public static class Global
+    {
+        public static int number = 13;
+    }
+
     class Uebungen
     {
-
+        public static void Start()
+        {
+            Console.WriteLine(Global.number);
+        }
     }
 }
