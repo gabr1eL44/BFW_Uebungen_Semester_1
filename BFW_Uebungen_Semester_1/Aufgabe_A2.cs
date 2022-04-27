6272cb7af785e0ea1bb9ad327d46ec3b8ae974d6
Stefan Karl <Stefan.Karl-1@wibizu.de>
2022-04-27 09:06:44 +0200

    comments

--- BFW_Uebungen_Semester_1/Aufgabe_A2.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_A2.cs
@@ -17,12 +17,14 @@ namespace BFW_Uebungen_Semester_1
             int jumps = 0;
             while (height >= 0.01)
             {
+                // Math.Round(..., 2) rundet nur height auf zwei Nachkommastellen
                 Console.WriteLine("Aktuelle Fallhöhe des Flummis beträgt: " + Math.Round(height, 2));
                 height /= 2;
                 jumps++;
                 Console.WriteLine("Booooooing!\n");
             }
 
+            // ....ToString("0.00") rundet nicht, sondern gibt nur die zwei ersten Nachkommastellen von height aus
             Console.WriteLine("Aktuelle Fallhöhe des Flummis beträgt: " + height.ToString("0.00"));
 
             Console.WriteLine("\nDer Flummi hat nach {0} Sprüngen seine finale Fallhöhe (<0.01 m) erreicht!", jumps);
