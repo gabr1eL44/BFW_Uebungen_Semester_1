7a13ab6541fb9c25a917bb464525c2379cf522df
gabr1eL44 <gabrielinc@gmx.de>
2022-04-27 09:55:26 +0200

    Schulaufgabe

---
+++ BFW_Uebungen_Semester_1/Schulaufgabe_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Schulaufgabe_1
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
