05e0a3f239f028faba57ca5b1d7da90fe7250113
gabr1eL44 <gabrielinc@gmx.de>
2022-04-30 16:48:09 +0200

    quicksort

--- BFW_Uebungen_Semester_1/Aufgabe_D3c.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_D3c.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace BFW_Uebungen_Semester_1
 {
@@ -14,7 +15,7 @@ namespace BFW_Uebungen_Semester_1
             for (int i = startPosition; i < startPosition + lengthSubArray; i++)
                 if (_numbers[i] < pivot)
                     newNumbers[countLeft++] = _numbers[i];
-                else if (_numbers[i] > pivot)
+                else if ((_numbers[i] >= pivot) && (i != startPosition))
                     newNumbers[lengthSubArray - ++countRight] = _numbers[i];
             newNumbers[countLeft] = pivot;
 
@@ -24,24 +25,63 @@ namespace BFW_Uebungen_Semester_1
             if (countLeft > 0)
                 Quicksort(ref _numbers, startPosition, countLeft);
             if (countRight > 0)
-                Quicksort(ref _numbers, countLeft + 1, countRight);  
+                Quicksort(ref _numbers, startPosition + countLeft + 1, countRight);  
         }
 
         public static void Start()
         {
-            const int N = 8;
+            const int N = 500000;
             int[] numbers = new int[N];
+            int[] numbersOLD = new int[N];
 
             Random rnd = new Random();
 
             for (int i = 0; i < numbers.Length; i++)
+            {
                 numbers[i] = rnd.Next(101);
+                numbersOLD[i] = numbers[i];
+            }
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
 
             Quicksort(ref numbers, 0, numbers.Length);
 
-            foreach (var element in numbers)
-                Console.Write(element + " ");
-            Console.WriteLine();
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}s {1:000}ms.", ts.Seconds, ts.Milliseconds);
+            Console.WriteLine("Quicksort mit {0} Zahlen => RunTime {1}", N, elapsedTime);
+
+            /* Vergleich mit Bubble Sort
+            int quickTime = ts.Seconds * 1000 + ts.Milliseconds;
+
+            stopWatch.Reset();
+            stopWatch.Start();
+
+            bool switched;
+            do
+            {
+                switched = false;
+                for (int i = 0; i < numbersOLD.Length - 1; i++)
+                    if (numbersOLD[i] > numbersOLD[i + 1])
+                    {
+                        int temp = numbersOLD[i];
+                        numbersOLD[i] = numbersOLD[i + 1];
+                        numbersOLD[i + 1] = temp;
+
+                        switched = true;
+                    }
+            } while (switched);
+
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+
+            elapsedTime = String.Format("{0:00}s {1:000}ms", ts.Seconds, ts.Milliseconds);
+            int bubbleTime = ts.Seconds * 1000 + ts.Milliseconds;
+            Console.WriteLine("Bubble Sort mit {0} Zahlen => RunTime {1}", N, elapsedTime);
+
+            Console.WriteLine("\nQuick Sort ist {0}-mal so schnell wie Bubble Sort!", Math.Round((double)bubbleTime/quickTime), 3);*/
         }
     }
 }
