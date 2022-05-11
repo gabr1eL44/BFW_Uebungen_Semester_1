31390d14fa54f27bfbd82e3c500f48f85f0929f7
gabr1eL44 <gabrielinc@gmx.de>
2022-05-11 20:48:49 +0200

    Aufgabe B2

---
+++ BFW_Uebungen_Semester_1/Aufgabe_B2a.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_B2a
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Bitte geben Sie eine Zeichenfolge ein: ");
+            string input = Console.ReadLine() + " ";
+
+            char c = input[0];
+            int i = 0;
+            string res = "";
+
+            foreach (var element in input)
+                if (c != element)
+                {
+                    res += "%" + i + "%" + c;
+                    c = element;
+                    i = 1;
+                }
+                else i++;
+
+            Console.WriteLine(res);
+
+            string temp = ""; bool count = false; int anzahl = 0; 
+
+            foreach (var element in res)
+            {
+                if (element == '%')
+                {
+                    if (count)
+                    {
+                        anzahl = Convert.ToInt32(temp);
+                        temp = "";
+                    }
+                    count = count ? false : true;
+                }
+                else if (count == false)
+                { 
+                    for (int j = 0; j < anzahl; j++)
+                            Console.Write(element);
+                }
+                else
+                    temp += element;
+            }
+        }
+    }
+}
---
+++ BFW_Uebungen_Semester_1/Aufgabe_B2b.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_B2b
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Bitte geben Sie eine Zeichenfolge ein: ");
+            string input = Console.ReadLine() + " ";
+
+            char c = input[0];
+            int i = 0;
+            string res = "";
+
+            foreach (var element in input)
+                if (c != element)
+                {
+                    if ((c != '%') && (c != '\\'))
+                        res += "%" + i + "%" + c;
+                    else 
+                        res += "%" + i + "%\\" + c; 
+                    c = element;
+                    i = 1;
+                }
+                else i++;
+          
+            Console.WriteLine(res);
+
+            string temp = ""; bool count = false, escape = false; int anzahl = 0;
+
+            foreach (var element in res)
+            {
+                if ((element == '%') && (escape == false))
+                {
+                    if (count)
+                    {
+                        anzahl = Convert.ToInt32(temp);
+                        temp = "";
+                    }
+                    count = count ? false : true;
+                }
+                else
+                {
+                    if ((element == '\\') && (escape == false))
+                        escape = true;
+                    else if (count == false)
+                    {
+                        for (int j = 0; j < anzahl; j++)
+                            Console.Write(element);
+                        escape = false;
+                    }
+                    else
+                        temp += element;
+                }
+            }
+        }
+    }
+}
