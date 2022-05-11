a177ac5642e2299bfc4929cd7c6f1e773b849972
gabr1eL44 <gabrielinc@gmx.de>
2022-05-11 16:16:40 +0200

    Aufgabe B2

--- BFW_Uebungen_Semester_1/Aufgabe_B1.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_B1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BFW_Uebungen_Semester_1
 {
@@ -6,7 +7,22 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
+            // Setting Encodings for € sign
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
 
+            Console.WriteLine("Bitte geben Sie den durchschnittlichen Einkaufspreis in Euro pro Quadratmeter ein: ");
+            decimal einkPreis = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie den durchschnittlichen Pachtpreis in Euro pro Quadratmeter ein: ");
+            decimal vermPreis = Convert.ToDecimal(Console.ReadLine());
+
+            decimal gewinn = vermPreis * 12;
+            Console.WriteLine("Ihr Gewinn/Verlust nach x Jahren: 5: {0} €  10: {1} €  15: {2} €  20: {3} €", gewinn * 5 - einkPreis, gewinn * 10 - einkPreis, gewinn * 15 - einkPreis, gewinn * 20 - einkPreis);
+
+            int jahre = (int)(einkPreis / (vermPreis * 12));
+            int monate = (int)((einkPreis / (vermPreis * 12)) % 12);
+            Console.WriteLine("\nSie werden die Gewinnschwelle nach {0} Jahren und {1} Monaten erreicht haben!", jahre, monate);
         }
     }
 }
