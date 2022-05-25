69fcb89fd1202f717bc981a612589bf88ed6ee94
gabr1eL44 <gabrielinc@gmx.de>
2022-05-25 14:37:03 +0200

    fix

---
+++ BFW_Uebungen_Semester_1/Aufgabe_C2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_C2
+    {
+        public static void Start()
+        {
+            int[] zutaten = new int[6]; int[] mengen = new int[] { 250, 125, 1, 75, 2, 1 };
+
+            Console.Write("Bitte geben Sie die Menge an Mehl ein (g):                     ");
+            zutaten[0] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte geben Sie die Menge an Butter (g) ein:                   ");
+            zutaten[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte geben Sie die Anzahl der Eier ein:                       ");
+            zutaten[2] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte geben die die Menge an Zucker (g) ein:                   ");
+            zutaten[3] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte geben Sie die Anzahl an Päckchen Vanilinzucker ein (Pk): ");
+            zutaten[4] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitte geben Sie die Anzahl an Prisen Salz ein:                 ");
+            zutaten[5] = Convert.ToInt32(Console.ReadLine());
+
+           int minimum = zutaten[0] / mengen[0];
+            for (int i = 1; i < 6; i++)
+            {
+                int j = zutaten[i] / mengen[i];
+                if (j < minimum)
+                    minimum = j;
+            }
+
+            Console.WriteLine("\nSie können maximal {0} Kekse backen", minimum * 5);
+        }
+    }
+}
--- BFW_Uebungen_Semester_1/Program.cs
+++ BFW_Uebungen_Semester_1/Program.cs
@@ -7,7 +7,7 @@ namespace BFW_Uebungen_Semester_1
         static void Main()
         {
             // Start Current Task
-            Aufgabe_C1b.Start();
+            Aufgabe_C2.Start();
 
             // Wait for KeyPress
             Console.WriteLine("\nDrücken Sie eine beliebige Taste, um dieses Fenster zu schließen.");
