468984bd4a99545c0434b8687987ce943376d325
gabr1eL44 <gabrielinc@gmx.de>
2022-05-25 12:22:20 +0200

    Aufgabe C1a

---
+++ BFW_Uebungen_Semester_1/Aufgabe_C1a.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_C1a
+    {
+        public static void Start()
+        {
+            Console.WriteLine("[1] Umrechnen von Kilokalorien (kcal) in KiloJoule (kJ)\n[2] Umrechnen von Kilojoule (kJ) in Kilokaoorien (kcal)");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.WriteLine("\nBitte geben Sie die Menge der Kalorien in (kcal) ein: ");
+                int kcal = Convert.ToInt32(Console.ReadLine());
+
+                double kJ = 4.18684 * kcal;
+                Console.WriteLine("{0} kcal sind genau {1} kJ.", kcal, kJ);
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("\nBitte geben Sie die Menge an Joule in (kJ) ein: ");
+                int kJ = Convert.ToInt32(Console.ReadLine());
+
+                double kcal = kJ / 4.18684;
+                Console.WriteLine("{0} kJ sind genau {1} kcal.", kJ, kcal);
+            }
+        }
+    }
+}
--- BFW_Uebungen_Semester_1/Program.cs
+++ BFW_Uebungen_Semester_1/Program.cs
@@ -7,7 +7,7 @@ namespace BFW_Uebungen_Semester_1
         static void Main()
         {
             // Start Current Task
-            Aufgabe_X3.Start();
+            Aufgabe_C1a.Start();
 
             // Wait for KeyPress
             Console.WriteLine("\nDrücken Sie eine beliebige Taste, um dieses Fenster zu schließen.");
