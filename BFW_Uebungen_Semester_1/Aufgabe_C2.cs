4f36a870efd6ba57825f3610f20436c8156379fe
gabr1eL44 <gabrielinc@gmx.de>
2022-05-26 18:04:03 +0200

    optimized

--- BFW_Uebungen_Semester_1/Aufgabe_C2.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_C2.cs
@@ -6,33 +6,26 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
-            int[] zutaten = new int[6]; int[] mengen = new int[] { 250, 125, 1, 75, 2, 1 };
+            int[] zutatenMenge = new int[6]; int[] mengen = new int[] { 250, 125, 1, 75, 2, 1 };
+            string[] zutaten = new string[] { "Mehl", "Butter", "Eier", "Zucker", "Vanilinzucker", "Salz"};
 
-            Console.Write("Bitte geben Sie die Menge an Mehl ein (g):                     ");
-            zutaten[0] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bitte geben Sie die Menge an Butter (g) ein:                   ");
-            zutaten[1] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bitte geben Sie die Anzahl der Eier ein:                       ");
-            zutaten[2] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bitte geben die die Menge an Zucker (g) ein:                   ");
-            zutaten[3] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bitte geben Sie die Anzahl an Päckchen Vanilinzucker ein (Pk): ");
-            zutaten[4] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bitte geben Sie die Anzahl an Prisen Salz ein:                 ");
-            zutaten[5] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < zutaten.Length; i++)
+            {
+                Console.Write("Bitte geben Sie die Menge an {0} ein: ", zutaten[i]);
+                zutatenMenge[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-           int maximum = zutaten[0] / mengen[0];
+           int maximum = zutatenMenge[0] / mengen[0];
             for (int i = 1; i < 6; i++)
             {
-                int j = zutaten[i] / mengen[i];
+                int j = zutatenMenge[i] / mengen[i];
                 if (j < maximum)
                     maximum = j;
             }
             Console.WriteLine("\nSie können maximal {0} Kekse backen", maximum * 5);
 
-            Console.WriteLine("\nDafür benötigen Sie die folgenden Mengen an Mehl, Butter, Eiern, Zucker, Vanilin und Salz..");
             for (int i = 0; i < 6; i++)
-                Console.WriteLine(maximum * mengen[i]);
+                Console.WriteLine("Benötigte Menge an {0}: {1}", zutaten[i], maximum * mengen[i]);
         }
     }
 }
