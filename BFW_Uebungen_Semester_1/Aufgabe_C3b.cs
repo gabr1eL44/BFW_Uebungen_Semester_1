4d785733c9de747c89ab4f45587b9d65a6c1138a
gabr1eL44 <gabrielinc@gmx.de>
2022-06-22 12:51:06 +0200

    Aufgabe_F1 added

---
+++ BFW_Uebungen_Semester_1/Aufgabe_C3b.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_C3b
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Bitte geben Sie die aktuelle Kaninchen-Population ein: ");
+            int population = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie die Zielpopulation ein: ");
+            int maxGeneration = Convert.ToInt32(Console.ReadLine());
+
+            int previous = 0, current = 1, counter = 0; 
+            while (current + previous < maxGeneration / population)
+            {
+                int j = current;
+                current += previous;
+                previous = j;
+
+               counter++;
+               Console.Write(current * population + " ");
+            }
+
+            Console.WriteLine((current + previous) * population + " \\\\Abbruch");
+            Console.WriteLine("\nEs sind {0} Schritte bis zur Zielpopulation nötig!", counter++);
+        }
+    }
+}
---
+++ BFW_Uebungen_Semester_1/Aufgabe_F1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Aufgabe_F1
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Bitte geben Sie eine (mindestens) 4-stellige Zahl ein:");
+            int zahl = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            zahl *= zahl;
+            Console.WriteLine("Ausgangszahl: " + zahl);
+
+            Console.WriteLine("Bitte geben Sie eine Zahl zwischen 2 und 10 ein:");
+            int teiler = Convert.ToInt32(Console.ReadLine());
+
+            int counter = 0, temp = zahl;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ausgangszahl: " + zahl);
+
+                temp /= teiler;
+                Console.WriteLine("Zwischenergebnis " + temp);
+                counter++;
+
+                Console.ReadLine();
+            } while (temp >= 1);
+
+            Console.WriteLine("Aufgabe erfolgreich gelöst!");
+            Console.WriteLine("Ausgangszahl: " + zahl);
+            Console.WriteLine("Anzahl Teilungen: " + counter);
+        }
+    }
+}
