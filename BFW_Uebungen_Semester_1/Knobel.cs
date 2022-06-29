8b59a84ad14ead77a59d2b7a9cff7103ec37a4ed
gabr1eL44 <gabrielinc@gmx.de>
2022-06-29 15:34:16 +0200

    knobel

---
+++ BFW_Uebungen_Semester_1/Knobel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Knobel
+    {
+        public static void Start()
+        {
+            // Lösung 1
+            Console.WriteLine("Hallo".Length);
+            Console.WriteLine();
+
+            // Lösung 2
+            Console.WriteLine('E' % 'A');
+            Console.WriteLine();
+
+            //Lösung 3
+            Console.WriteLine("ooooo");
+            Console.WriteLine("o");
+            Console.WriteLine("ooooo");
+            Console.WriteLine("    o");
+            Console.WriteLine("ooooo");
+
+            int i = Convert.ToByte(true);
+            Console.WriteLine();
+            Console.WriteLine((i<<i<<i)|i);
+        }
+    }
+}
