308cc970691abdf7dec0a5aa7399a55232de0999
gabr1eL44 <gabrielinc@gmx.de>
2022-07-05 19:16:31 +0200

    schulaufgabe

--- BFW_Uebungen_Semester_1/Program.cs
+++ BFW_Uebungen_Semester_1/Program.cs
@@ -7,7 +7,7 @@ namespace BFW_Uebungen_Semester_1
         static void Main()
         {
             // Start Current Task
-            Knobel.Start();
+            Schulaufgabe2a.Start();
 
             // Wait for KeyPress
             Console.WriteLine("\nDrücken Sie eine beliebige Taste, um dieses Fenster zu schließen.");
---
+++ BFW_Uebungen_Semester_1/Schulaufgabe2b.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_1
+{
+    class Schulaufgabe2b
+    {
+        public static void Start()
+        {
+            
+        }
+    }
+}
