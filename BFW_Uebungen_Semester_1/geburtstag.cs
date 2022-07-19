2d5310ed1aafea171fae927a260dda11d01cf339
gabr1eL44 <gabrielinc@gmx.de>
2022-07-19 08:20:47 +0200

    fix

--- BFW_Uebungen_Semester_1/geburtstag.cs
+++ BFW_Uebungen_Semester_1/geburtstag.cs
@@ -6,7 +6,7 @@ namespace BFW_Uebungen_Semester_1
     {
         public static void Start()
         {
-            int m = 2, t = 14, j = 2023;
+            int t = 14, m = 2, j = 2023;
             if (((m - 8) * 100 + (t - 15) >= 0 && j == 2022) || ((m - 2) * 100 + (t - 15) <= 0 && j == 2023))
                 Console.WriteLine("Sie haben Geburtstag!");
             else
